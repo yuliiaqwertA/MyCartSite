3ef50b1e601dc10acca17d4d92dffb4cb184ff94
Admin <yulya777@email.ua>
2023-08-07 20:01:52 -0700

    header,hamburgermenu

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const partHeader = document.querySelector('.partHeader');
+const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
-navToggle.addEventListener("click", () => {
-	const visibility = partHeader.getAttribute("data-visible")
-
-	if (visibility === "false") {
-		partHeader.setAttribute("data-visible", true);
-
-	} else if (visibility === "true") {
-		partHeader.setAttribute("data-visible", false);
-	}
-
+navToggle.addEventListener('click', () => {
+ const visibility = primaryNav.getAttribute('data-visible');
+ 
+ if (visibility === "false") {
+  primaryNav.setAttribute('data-visible', true);
+  navToggle.setAttribute('aria-expanded', true);
+ } else if (visibility === "true") {
+  primaryNav.setAttribute('data-visible', false);
+  navToggle.setAttribute('aria-expanded', false);
+ }
 });
